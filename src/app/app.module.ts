680bc70fb43a6a07acf58e00b63cb20b477b4af4
Zachary Morgan <zsmorgan21@gmail.com>
2018-03-04 18:08:23 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatSelectModule,
+  MatCardModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 
 import {AppComponent} from './app.component';
-import { JobApplicationService } from './services/job-application.service';
-import { MatNativeDateModule } from '@angular/material/core';
+import {JobApplicationService} from './services/job-application.service';
+import {MatNativeDateModule} from '@angular/material/core';
 import {JobApplicationComponent} from './job-application/job-application.component';
 import {JobApplicationDetailComponent} from './job-application-detail/job-application-detail.component';
-import { ApplicationFilterPipe } from './pipes/application-filter.pipe';
-import { AppRoutingModule } from './app-routing.module';
-import { JobApplicationListComponent } from './job-application-list/job-application-list.component';
+import {ApplicationFilterPipe} from './pipes/application-filter.pipe';
+import {AppRoutingModule} from './app-routing.module';
+import {JobApplicationListComponent} from './job-application-list/job-application-list.component';
 
 
 @NgModule({
